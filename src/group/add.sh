9ea555d2bf98c3f086b30f8200863486608c0056
José de Melo <josedemelo.if@gmail.com>
2019-11-20 11:40:54 -0300

    [UP] Funcionalidade para adicionar grupos finalizada

--- src/group/add.sh
+++ src/group/add.sh
@@ -1,8 +1,110 @@
 #!/bin/bash
 
 
-uid=$( dialog --stdout                         \
+password=$(cat .password)
+
+cn=$( dialog --stdout                         \
    --backtitle 'DLDAP - Adicionar Grupo'      \
    --title 'Novo Grupo:'                         \
-   --inputbox '\n\nUsername (uid): '  \
+   --inputbox '\n\nNome (cn): '  \
    13 50 )
+
+[ $? -ne 0 ] && src/dldap-groups.sh && exit
+
+if [ -z "$cn" ] ;
+then
+	src/message.sh "DLDAP - Adicionar Grupo" "Erro" "O nome do grupo não pode ser nulo!"
+	#src/dldap-groups.sh
+	exit
+fi
+
+ldapsearch -LLL -x -D "cn=admin,dc=jose,dc=labredes,dc=info" -H ldap://ldap1 -b "dc=jose,dc=labredes,dc=info" "(objectClass=posixGroup)" -w $password | grep "cn: $cn" > /dev/null
+
+if [ $? -eq 0 ] ;
+then
+	src/message.sh "DLDAP - Adicionar Grupo" "Erro" "O nome fornecido já está sendo utilizado na base!"	
+	#src/dldap-groups.sh
+	exit
+fi
+
+
+desc=$( dialog --stdout                         \
+   --backtitle 'DLDAP - Adicionar Grupo'      \
+   --title 'Novo Grupo:'                         \
+   --inputbox '\n\nDescrição do grupo: '  \
+   13 50 )
+
+[ $? -ne 0 ] && src/dldap-groups.sh && exit
+
+if [ -z "$desc" ] ;
+then
+        src/message.sh "DLDAP - Adicionar Grupo" "Erro" "A descrição do grupo não pode ser vazia!"
+        #src/dldap-groups.sh
+        exit
+fi
+
+users=$(ldapsearch -LLL -x -D "cn=admin,dc=jose,dc=labredes,dc=info" -H ldap://ldap1 -b "dc=jose,dc=labredes,dc=info" "(objectClass=posixAccount)" uid -w $password | grep uid: | cut -d" " -f2)
+
+
+LIST=()
+
+for user in $(echo $users)
+do
+        DESC=''
+        LIST+=( $user "$DESC" off)
+done
+
+users=$( dialog --stdout \
+        --backtitle "DLDAP - Adicionar Grupo" \
+        --title "Selecionar usuários" \
+        --separate-output \
+        --checklist 'Selecione um ou mais usuários para adicionar ao grupo:' 0 40 0 \
+        "${LIST[@]}" \
+        )
+
+
+gid=$(expr $(src/group/get-greatest-gid.sh) + 1)
+
+
+if [ -z "$users" ];
+then
+	src/message.sh "DLDAP - Adicionar Grupo" "Erro" "Selecione pelo menos um usuário para adicionar ao grupo $cn!"
+        #src/dldap-groups.sh
+        exit
+
+fi
+
+cat src/group/ldifs/add-group.ldif | sed "s/<cn>/$cn/" | sed "s/<desc>/$desc/" | sed "s/<gid>/$gid/" >> $cn.ldif
+
+
+for user in $users
+do
+	echo "uniqueMember: uid=$user,ou=Usuarios,dc=jose,dc=labredes,dc=info" >> $cn.ldif
+done
+
+echo "" >> $cn.ldif
+
+ldapadd -x -D 'cn=admin,dc=jose,dc=labredes,dc=info' -H ldap://ldap1 -f $cn.ldif -w $password >> logs/groupadd.log
+
+
+echo "ADD GROUP $cn\n" >> logs/groupadd.log
+mv $cn.ldif logs/ldifs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/group/get-greatest-gid.sh
+++ src/group/get-greatest-gid.sh
@@ -6,15 +6,15 @@ password=$(cat .password)
 
 gids=$(ldapsearch -LLL -x -D 'cn=admin,dc=jose,dc=labredes,dc=info' -H ldap://ldap1 -b 'ou=Grupos,dc=jose,dc=labredes,dc=info' '(objectClass=posixGroup)' gidNumber -w $password | grep gidNumber: | cut -d" " -f2)
 
-base_gid=2000
+base_gid="2000"
 
 for gid in $gids
 do
-	if [ gid -ne 10000 ]; then
-		if [ gid -ne base_gid ]; then
-			base_gid=gid
+	if [ $gid -le 10000 ]; then
+		if [ $gid -gt $base_gid ]; then
+			base_gid=$gid
 		fi
 	fi
 done
 
-
+echo $base_gid

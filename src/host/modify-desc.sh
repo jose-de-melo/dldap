d5641e49bed6bc9e8ebc59d105653e29ae8460fe
José de Melo <josedemelo.if@gmail.com>
2019-11-27 09:51:07 -0300

    [UP] Alterar descrição de máquinas

---
+++ src/host/modify-desc.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+password=$( cat .password )
+host=$1
+
+desc=$( dialog --stdout                         \
+   --backtitle 'DLDAP - Alterar Máquina'      \
+   --title "Alterar descrição da máquina: $host"                         \
+   --inputbox '\n\nInsira a nova descrição: '  \
+   13 50 )
+
+if [ $? -ne 0 ];then
+        src/dldap-hosts.sh
+        exit
+fi
+
+if [ -z "$desc" ];
+then
+        dialog --backtitle 'DLDAP - Alterar Máquina' --title 'Erro!' --msgbox 'O novo valor não pode estar vazio!' 6 40
+        src/dldap-hosts.sh
+        exit
+fi
+
+desc=$(echo $desc | sed 'y/áÁàÀãÃâÂéÉêÊíÍóÓõÕôÔúÚçÇ/aAaAaAaAeEeEiIoOoOoOuUcC/')
+
+cat src/host/ldifs/modify.ldif | sed "s/<attr>/description/" | sed "s/<host>/$host/" | sed "4c\description: $desc" > $host.ldif
+
+ldapmodify -x -D 'cn=admin,dc=jose,dc=labredes,dc=info' -H ldap://ldap1 -f $host.ldif -w $password >> logs/modify-hosts.log
+
+echo -e "MODIFY DESCRIPTION FROM HOST $host TO $desc \n" >> logs/modify-groups.log
+
+mv $host.ldif logs/ldifs
+
+dialog                                            \
+  --backtitle 'DLDAP - Alterar Máquina'                 \
+   --title 'Sucesso!'                             \
+   --msgbox "A descrição da máquina $host foi atualizada."  \
+   6 40
+
+src/dldap-hosts.sh
---
+++ src/host/modify.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+password=$(cat .password)
+
+hosts=$(ldapsearch -LLL -x -D "cn=admin,dc=jose,dc=labredes,dc=info" -H ldap://ldap1 -b "ou=Maquinas,dc=jose,dc=labredes,dc=info" "objectClass=nisNetGroup" -w $password | grep cn: | cut -d" " -f2)
+
+
+LIST=()
+
+for h in $(echo $hosts)
+do
+        DESC=''
+        LIST+=( $h "$DESC" off)
+done
+
+host=$( dialog --stdout \
+        --backtitle "DLDAP - Gerenciamento de Máquinas" \
+        --title "Alterar Máquina" \
+        --radiolist '' 0 40 0 \
+        "${LIST[@]}" \
+       )
+
+if [ $? -ne 0 ]; then
+        src/dldap-hosts.sh
+        exit
+fi
+
+cn=$host
+
+op=$( dialog --cancel-label "Voltar" --backtitle 'DLDAP - Gerenciamento de Máquinas' \
+        --stdout                 \
+        --menu 'Selecione uma opção:'       \
+        0 0 0                    \
+        1 "Alterar Descrição : $cn" \
+        2 "Alterar uma Interface : $cn" \
+	3 "Adicionar uma Interface : $cn" \
+	4 "Remover uma Interface : $cn" \
+        0 'Voltar'  )
+
+if [ $? -ne 0 ];then
+        src/dldap-hosts.sh
+        exit
+fi
+
+case "$op" in
+        1) src/host/modify-desc.sh $cn;;
+        2) src/host/modify-if.sh $cn;;
+	3) src/host/add-if.sh ;;
+	4) src/host/remove-if.sh ;;
+        0) ./dldap-hosts.sh ;;
+esac
